4a20a03fa71507562a89235578df3caa8c7e8982
NekitoSP <nekitosp@gmail.com>
2021-06-05 11:14:03 +0500

    photoapi: добавил модель UserPhoto, настроил админку для модели

--- src/photos/admin.py
+++ src/photos/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import UserPhoto
+
+
+class UserPhotoInline(admin.StackedInline):
+    model = UserPhoto
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserPhotoInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- src/photos/models.py
+++ src/photos/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserPhoto(models.Model):
+    """
+    Модель-расширение стандартного User для проверки возможности загрузки изображений.
+
+
+    Альтернативные решения:
+
+    1) замена существующей модели и настройка AUTH_USER_MODEL
+    2) т.к. задача схожа с проверкой прав доступа, при наличии системы прав доступа в проекте
+        можно было добавить permission типа "can_add_photo" и проверять его наличие при работе с фотографиями
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    can_add_photo = models.BooleanField("Возможность загружать фотографии", default=True, blank=False, null=False)
